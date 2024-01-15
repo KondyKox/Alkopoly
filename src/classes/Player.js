f4dc4acb88e32e9238681d473d544cc245a24077
Konrad <kondykox@gmail.com>
2024-01-15 20:40:32 +0100

    player class

---
+++ src/classes/Player.js
@@ -0,0 +1,23 @@
+class Player {
+  constructor(name, pawn, position) {
+    this.name = name;
+    this.pawn = pawn;
+    this.position = position;
+    this.money = 1000;
+  }
+
+  // Move on board
+  move(steps) {
+    this.position += steps;
+  }
+
+  // Add money
+  addMoney(amount) {
+    this.money += amount;
+  }
+
+  // Spent money
+  substractMoney(amount) {
+    this.money -= amount;
+  }
+}
